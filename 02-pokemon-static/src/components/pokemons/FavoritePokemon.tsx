d4b2b2d867777ed0d5835f14b69e92220f5153ee
jcpmbhc <jpineda@banhcafe.hn>
2026-06-10 10:58:31 -0600

    #55 Mostrar y remover favoritos

---
+++ 02-pokemon-static/src/components/pokemons/FavoritePokemon.tsx
@@ -0,0 +1,8 @@
+import type { FavoritePokemon } from "@/interfaces/favorite-pokemon";
+import type { Component } from "solid-js";
+
+interface Props {
+  pokemon: FavoritePokemon;
+}
+
+export const FacoritePokemonCard: Component<Props> = () => {};
--- 02-pokemon-static/src/components/pokemons/FavoritePokemons.tsx
+++ 02-pokemon-static/src/components/pokemons/FavoritePokemons.tsx
@@ -1,3 +1,4 @@
+import { FacoritePokemonCard } from "@/components/pokemons/FavoritePokemonCard";
 import type { FavoritePokemon } from "@/interfaces/favorite-pokemon";
 import { createSignal, For } from "solid-js";
 
@@ -14,7 +15,9 @@ export const FavoritePokemons = () => {
 
   return (
     <div class="grid grid-cols-2 sm:grid-cols-4">
-      <For each={pokemons()}>{(pokemon) => <h1>{pokemon.name}</h1>}</For>
+      <For each={pokemons()}>
+        {(pokemon) => <FacoritePokemonCard pokemon={pokemon} />}
+      </For>
     </div>
   );
 };
