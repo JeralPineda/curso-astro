d1427529d67ec0218f6f5aaffc7c6202286a0fd7
jcpmbhc <jpineda@banhcafe.hn>
2026-06-11 10:38:41 -0600

    #71 Imagenes en colecciones

--- 03-blog/src/content.config.ts
+++ 03-blog/src/content.config.ts
@@ -7,18 +7,21 @@ import { z } from "astro/zod";
 // Define un `loader` y un `schema` para cada colección
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
-  schema: z.object({
-    title: z.string(),
-    date: z.coerce.date(),
-    description: z.string(),
-    image: z.string().optional(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.coerce.date(),
+      description: z.string(),
+      image: image(), //.refine((img) => img.width < 1200, {
+      // message: "Image should be lower than 1200px",
+      // }),
 
-    // Relación
-    author: z.string(),
+      // Relación
+      author: z.string(),
 
-    // Relación
-    tags: z.array(z.string()),
-  }),
+      // Relación
+      tags: z.array(z.string()),
+    }),
 });
 
 // Exporta un solo objeto `collections` para registrar tus colecciones
