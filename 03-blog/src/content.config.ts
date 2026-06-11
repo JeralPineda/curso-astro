18e8aefd7dc1c2d88347e8707379d2833ca7154c
Jeral Pineda <jeral.cris@gmail.com>
2026-06-10 22:12:02 -0600

    fix: migrando a collections

---
+++ 03-blog/src/content.config.ts
@@ -0,0 +1,21 @@
+// Importa el cargador glob
+import { glob } from "astro/loaders";
+// Importa utilidades de `astro:content` y `astro/zod`
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+
+// Define un `loader` y un `schema` para cada colección
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.string().optional(),
+    tags: z.array(z.string()),
+  }),
+});
+
+// Exporta un solo objeto `collections` para registrar tus colecciones
+export const collections = { blog };
