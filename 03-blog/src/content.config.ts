97da9136177da5f402ad14d92f33012a26def58c
Jeral Pineda <jeral.cris@gmail.com>
2026-06-11 21:50:38 -0600

    #81-82 Listado de autores

--- 03-blog/src/content.config.ts
+++ 03-blog/src/content.config.ts
@@ -32,6 +32,11 @@ const author = defineCollection({
     z.object({
       name: z.string(),
       avatar: image(),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string(),
     }),
 });
 
