79b04d010377a26cb410636faf9e6152edb748e0
Jeral Pineda <jeral.cris@gmail.com>
2026-06-13 11:47:00 -0600

    #106 SSR Adapters

--- 05-astro-http/src/pages/api/posts/index.ts
+++ 05-astro-http/src/pages/api/posts/index.ts
@@ -4,6 +4,8 @@ import { getCollection } from "astro:content";
 export const GET: APIRoute = async ({ params, request }) => {
   const posts = await getCollection("blog");
 
+  console.log("🚀 index.ts -> #7 ~ request:", request);
+
   return new Response(JSON.stringify(posts), {
     status: 201,
     headers: {
