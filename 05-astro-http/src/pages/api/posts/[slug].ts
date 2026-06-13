993328b2601b7e41279c7190a2fb2ebb9c82fac8
Jeral Pineda <jeral.cris@gmail.com>
2026-06-13 14:26:41 -0600

    #110 Post, Put, Delete Endpoints

--- 05-astro-http/src/pages/api/posts/[slug].ts
+++ 05-astro-http/src/pages/api/posts/[slug].ts
@@ -19,3 +19,71 @@ export const GET: APIRoute = async ({ params, request }) => {
     status: 200,
   });
 };
+
+export const POST: APIRoute = async ({ params, request }) => {
+  const body = await request.json();
+
+  return new Response(
+    JSON.stringify({
+      method: "POST",
+      ...body,
+    }),
+    {
+      status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+};
+
+export const PUT: APIRoute = async ({ params, request }) => {
+  const body = await request.json();
+
+  return new Response(
+    JSON.stringify({
+      method: "PUT",
+      ...body,
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+};
+
+export const PATCH: APIRoute = async ({ params, request }) => {
+  const body = await request.json();
+
+  return new Response(
+    JSON.stringify({
+      method: "PATCH",
+      ...body,
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+};
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+  const { slug } = params;
+
+  return new Response(
+    JSON.stringify({
+      method: "DELETE",
+      slug,
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+};
