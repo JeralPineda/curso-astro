c706908388c00f259cb723d26b1e152c25e7e5f2
Jeral Pineda <jeral.cris@gmail.com>
2026-06-13 13:13:03 -0600

    #108 Query Parameters - Opcionales

--- 05-astro-http/src/pages/api/posts/index.ts
+++ 05-astro-http/src/pages/api/posts/index.ts
@@ -2,12 +2,39 @@ import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
 export const GET: APIRoute = async ({ params, request }) => {
+  const url = new URL(request.url);
+  const slug = url.searchParams.get("slug");
+
+  if (slug) {
+    const postFiltered = await getCollection("blog", ({ data }) =>
+      data.title.toLowerCase().includes(slug.toLowerCase()),
+    );
+    // puede ser undefined si no existe
+    // esto para que llegue al 404 porque siempre devuelve un array vació o con 1 elemento.
+    const post = postFiltered[0];
+
+    if (post) {
+      return new Response(JSON.stringify(post), {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+
+    return new Response(JSON.stringify({ msg: `Post ${slug} not found` }), {
+      status: 404,
+      statusText: "Not Found",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   const posts = await getCollection("blog");
 
-  console.log("🚀 index.ts -> #7 ~ request:", request);
-
   return new Response(JSON.stringify(posts), {
-    status: 201,
+    status: 200,
     headers: {
       "Content-Type": "application/json",
     },
