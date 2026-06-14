26b01cf42974e7a28a450039e66c0f40662106f9
Jeral Pineda <jeral.cris@gmail.com>
2026-06-14 14:46:58 -0600

    #130 Put - Incrementar la cantidad de likes

--- 05-astro-http/src/pages/api/likes/[id].ts
+++ 05-astro-http/src/pages/api/likes/[id].ts
@@ -28,3 +28,30 @@ export const GET: APIRoute = async ({ params, request }) => {
     },
   });
 };
+
+export const PUT: APIRoute = async ({ params, request }) => {
+  const postId = params.id ?? "";
+  const { likes } = await request.json();
+
+  const posts = await db.select().from(Posts).where(eq(Posts.id, postId));
+
+  if (posts.length === 0) {
+    const newPost = {
+      id: postId,
+      title: "Post not found",
+      likes: 0,
+    };
+
+    await db.insert(Posts).values(newPost);
+    posts.push(newPost);
+  }
+
+  const post = posts.at(0)!;
+  post.likes = post.likes + likes;
+
+  await db.update(Posts).set(post).where(eq(Posts.id, postId));
+
+  return new Response("OK", {
+    status: 200,
+  });
+};
