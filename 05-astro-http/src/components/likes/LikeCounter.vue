77ad4981b50f43e9c893e2855bfe9a585658c6b1
Jeral Pineda <jeral.cris@gmail.com>
2026-06-14 16:07:26 -0600

    #131 Like Counter - Componente con Vue.js

---
+++ 05-astro-http/src/components/likes/LikeCounter.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="likePost">Like counter</button>
+</template>
+
+<script lang="ts" setup>
+const likePost = () => {
+  console.log("+1 like");
+};
+</script>
+
+<style scoped>
+button {
+  background-color: #5351bc;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+
+button:hover {
+  background-color: #4a3f90;
+}
+</style>
