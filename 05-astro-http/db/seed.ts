688695f5955dc92222ec510534b24c9a2641f669
Jeral Pineda <jeral.cris@gmail.com>
2026-06-13 20:53:41 -0600

    #117 Crear tabla de clientes

--- 05-astro-http/db/seed.ts
+++ 05-astro-http/db/seed.ts
@@ -1,8 +1,11 @@
-import { db } from "astro:db";
+import { db, Clients } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-  // TODO
+  await db.insert(Clients).values([
+    { id: 1, name: "Juan", age: 30, isActive: true },
+    { id: 2, name: "Maria", age: 25, isActive: false },
+  ]);
 
   console.log("Seed executed");
 }
