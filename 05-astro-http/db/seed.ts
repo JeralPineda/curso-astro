99909aa9e0a1a7ad151fd3a29966b3ea02da2d43
Jeral Pineda <jeral.cris@gmail.com>
2026-06-14 13:34:19 -0600

    #128 Estructura y Seed

--- 05-astro-http/db/seed.ts
+++ 05-astro-http/db/seed.ts
@@ -1,4 +1,5 @@
-import { db, Clients } from "astro:db";
+import { getCollection } from "astro:content";
+import { db, Clients, Posts } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -6,6 +7,16 @@ export default async function seed() {
     { id: 1, name: "Juan", age: 30, isActive: true },
     { id: 2, name: "Maria", age: 25, isActive: false },
   ]);
+
+  const posts = await getCollection("blog");
+
+  await db.insert(Posts).values(
+    posts.map((post) => ({
+      id: post.id,
+      title: post.data.title,
+      likes: Math.round(Math.random() * 100),
+    })),
+  );
 
   console.log("Seed executed");
 }
