cc1eee347bcf4b839d11f3941b982992ff13eb36
Jeral Pineda <jeral.cris@gmail.com>
2026-06-13 21:57:08 -0600

    #119 CRUD con Astro DB - POST y GET

--- 05-astro-http/src/pages/api/clients/index.ts
+++ 05-astro-http/src/pages/api/clients/index.ts
@@ -1,29 +1,46 @@
 import type { APIRoute } from "astro";
+import { Clients, db } from "astro:db";
 
 export const GET: APIRoute = async ({ params, request }) => {
-  return new Response(
-    JSON.stringify({
-      method: "GET",
-    }),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
+  const clients = await db.select().from(Clients).all();
+
+  return new Response(JSON.stringify(clients), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
     },
-  );
+  });
 };
 
 export const POST: APIRoute = async ({ params, request }) => {
-  return new Response(
-    JSON.stringify({
-      method: "POST",
-    }),
-    {
-      status: 201,
-      headers: {
-        "Content-Type": "application/json",
+  try {
+    const { id, ...body } = await request.json();
+
+    const { lastInsertRowid } = await db.insert(Clients).values(body);
+
+    return new Response(
+      JSON.stringify({
+        id: Number(lastInsertRowid),
+        ...body,
+      }),
+      {
+        status: 201,
+        headers: {
+          "Content-Type": "application/json",
+        },
       },
-    },
-  );
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        msg: `Error: ${error}`,
+      }),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
 };
