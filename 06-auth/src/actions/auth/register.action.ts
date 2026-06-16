0c817fd5189bfc7ca9d9f7a0e88ea42a485b1693
jcpmbhc <jpineda@banhcafe.hn>
2026-06-16 09:21:46 -0600

    #149 Cookies: Recordar usuario

--- 06-auth/src/actions/auth/register.action.ts
+++ 06-auth/src/actions/auth/register.action.ts
@@ -9,21 +9,16 @@ export const registerUser = defineAction({
     password: z.string().min(6),
     remember_me: z.boolean().optional(),
   }),
-  handler: async ({ name, email, password, remember_me }) => {
-    console.log(
-      "🚀 register.action.ts -> #13 ~ form",
-      JSON.stringify(
-        {
-          name,
-          email,
-          password,
-          remember_me,
-        },
-        null,
-        2,
-      ),
-    );
+  handler: async ({ name, email, password, remember_me }, { cookies }) => {
+    if (remember_me) {
+      cookies.set("email", email, {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), // 1 año
+        path: "/",
+      });
+    } else {
+      cookies.delete("email", { path: "/" });
+    }
 
-    return true;
+    return { ok: true, msg: "Usuario registrado" };
   },
 });
