0a5770d787ff0cac97870ba59764ea3431e132cd
jcpmbhc <jpineda@banhcafe.hn>
2026-06-16 14:33:31 -0600

    #153 Logout - Cerrar sesion

--- 06-auth/src/actions/index.ts
+++ 06-auth/src/actions/index.ts
@@ -1,6 +1,8 @@
+import { logout } from "@/actions/auth/logout.action";
 import { registerUser } from "./auth/register.action";
 
 export const server = {
   // actions
   registerUser,
+  logout,
 };
