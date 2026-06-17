64b5ab932833d6ae04fb2211d3c20818f965ae8d
Jeral Pineda <jeral.cris@gmail.com>
2026-06-16 20:21:27 -0600

    refactor: use environment variable for apiKey

--- 06-auth/src/firebase/config.ts
+++ 06-auth/src/firebase/config.ts
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_ya6mYMLJBYgWpjvE5LyaIjLtaMT8vok",
+  apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: "astro-authentication-f8eee.firebaseapp.com",
   projectId: "astro-authentication-f8eee",
   storageBucket: "astro-authentication-f8eee.firebasestorage.app",
