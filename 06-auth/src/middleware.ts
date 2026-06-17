4322c3f01e56a6163745912bf5277d54c06bc62e
Jeral Pineda <jeral.cris@gmail.com>
2026-06-16 22:56:51 -0600

    #159 Proteger rutas desde Middleware

--- 06-auth/src/middleware.ts
+++ 06-auth/src/middleware.ts
@@ -1,11 +1,11 @@
-import type { MiddlewareNext } from "astro";
 import { defineMiddleware } from "astro:middleware";
 import { firebase } from "./firebase/config";
 
 const privateRoutes = ["/protected"];
+const publicRoutes = ["/login", "/register"];
 
 export const onRequest = defineMiddleware(
-  async ({ url, request, locals }, next) => {
+  async ({ url, request, locals, redirect }, next) => {
     const isLoggedIn = !!firebase.auth.currentUser;
     const user = firebase.auth.currentUser;
 
@@ -18,6 +18,14 @@ export const onRequest = defineMiddleware(
         name: user.displayName!,
         emailVerified: user.emailVerified,
       };
+    }
+
+    if (!isLoggedIn && privateRoutes.includes(url.pathname)) {
+      return redirect("/");
+    }
+
+    if (isLoggedIn && publicRoutes.includes(url.pathname)) {
+      return redirect("/");
     }
 
     return next();
