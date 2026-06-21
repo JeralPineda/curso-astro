24bd3a84c591f1687d223636adf4e27b139c786f
Jeral Pineda <jeral.cris@gmail.com>
2026-06-21 13:33:46 -0600

    #199 Compartir informacion entre islas - NanoStores

--- 07-store/src/components/cart/CartCounter.tsx
+++ 07-store/src/components/cart/CartCounter.tsx
@@ -1,8 +1,20 @@
+import { itemsInCart } from "@/store/cart.store";
+import { CartCookiesClient } from "@/utils/cart-cookies";
+import { useStore } from "@nanostores/react";
+import { useEffect } from "react";
+
 export const CartCounter = () => {
+  const $itemsInCart = useStore(itemsInCart);
+
+  useEffect(() => {
+    const cart = CartCookiesClient.getCart();
+    itemsInCart.set(cart.length);
+  }, []);
+
   return (
     <a href="/cart" className="relative inline-block">
       <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-blue-600 text-xs text-white">
-        3
+        {$itemsInCart}
       </span>
 
       <svg
